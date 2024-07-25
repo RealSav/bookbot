df0fc5e43b7b39ef476b47a0d9ea0c6f5e918a34
RealSav <avidc4@gmail.com>
2024-07-24 22:14:24 -0400

    Feature: Created word_count function to take text from provided books and returns the count of words

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+# 1. Open the file
+# 2. Read the contents
+# 3. Print the contents
+# Add a new function that takes the txt from the book as a string and returns the # of words in the string
+# Add a print() then run code to make sure it does as expected, expecting 77986
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
-
+        
+def word_count():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        print(len(words))
+        
 if __name__ == "__main__":
     main()
+    word_count()
